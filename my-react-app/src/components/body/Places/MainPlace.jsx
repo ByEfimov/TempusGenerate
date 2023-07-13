141593d0d7138c71f5582831a6b59ccd3d1f5e17
Efimov <nikitacanai@yandex.ru>
2023-07-13 21:06:05 +0700

    update

--- my-react-app/src/components/body/Places/MainPlace.jsx
+++ my-react-app/src/components/body/Places/MainPlace.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+import userData from "../../../storage/storage";
+
 function MainPlace() {
+  const [UserData, setUserdata] = useState(userData);
+
   return (
     <section className="Main-place place" id="modetoday">
-      Сегодня
+      {UserData[0].userTasksToDay.map(function (task) {
+        return (
+          <div className="Task" key={task.id}>
+            <input id="CheckBox" className="CheckBox" type="checkbox" />
+            <label className="Label">{task.TaskName}</label>
+            <div className="date">Сегодня</div>
+          </div>
+        );
+      })}
     </section>
   );
 }
---
+++ my-react-app/src/storage/storage.jsx
@@ -0,0 +1,30 @@
+const userData = [
+  {
+    id: 0,
+    userProperties: {
+      login: "Nikita",
+      Email: "nikita@mail.com",
+      Password: "lol",
+      TeamCode: 22,
+      Role: "admin",
+      InviteCode: "22",
+    },
+    userTasksToDay: [
+      { id: 0, TaskName: "Создать привет", TaskSatus: "Make", TaskPriority: 3 },
+      {
+        id: 1,
+        TaskName: "Приготовить тортик",
+        TaskSatus: "Make",
+        TaskPriority: 2,
+      },
+      {
+        id: 2,
+        TaskName: "ГотовоГотовоГотовоГотово",
+        TaskSatus: "Done",
+        TaskPriority: 1,
+      },
+    ],
+  },
+];
+
+export default userData;
