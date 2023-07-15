e5702950edaf2752a5350873fcecdb520af41f02
Efimov <nikitacanai@yandex.ru>
2023-07-15 23:09:29 +0700

    fix hahah

--- my-react-app/src/components/body/components/nohavetasks.jsx
+++ my-react-app/src/components/body/components/nohavetasks.jsx
@@ -2,7 +2,7 @@ function NoHaveTasks() {
   return (
     <div className="noHaveTasks">
       <div className="noHaveTasks-body">
-        <div className="title">Здесь еще нет задачь</div>
+        <div className="title">Здесь еще нет задач</div>
         <button className="addtask">Создать задачу</button>
       </div>
     </div>
