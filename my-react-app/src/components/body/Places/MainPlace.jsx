f85a930bd20114ca4f9bf050f1da6944c5f36973
Efimov <nikitacanai@yandex.ru>
2023-07-15 23:25:49 +0700

    fix

--- my-react-app/src/components/body/Places/MainPlace.jsx
+++ my-react-app/src/components/body/Places/MainPlace.jsx
@@ -23,7 +23,7 @@ function MainPlace(props) {
     const date = new Date();
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, "0");
-    const day = String(date.getDate() + 2).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
     const formattedDate = `${year}-${month}-${day}`;
     return formattedDate;
   }
--- my-react-app/src/storage/storage.jsx
+++ my-react-app/src/storage/storage.jsx
@@ -15,7 +15,7 @@ const usersData = [
         TaskName: "Создать привет",
         TaskSatus: "Make",
         TaskPriority: 1,
-        date: "2023-07-17",
+        date: "2023-07-15",
       },
 
       {
@@ -23,21 +23,21 @@ const usersData = [
         TaskName: "Приготовить тортик",
         TaskSatus: "Make",
         TaskPriority: 2,
-        date: "2023-07-17",
+        date: "2023-07-15",
       },
       {
         id: 3,
         TaskName: "ГотовоГотовоГотовоГотово",
         TaskSatus: "Done",
         TaskPriority: 3,
-        date: "2023-07-17",
+        date: "2023-07-15",
       },
       {
         id: 0,
         TaskName: "Создать привет",
         TaskSatus: "Make",
         TaskPriority: 1,
-        date: "2023-07-18",
+        date: "2023-07-16",
       },
 
       {
@@ -45,14 +45,21 @@ const usersData = [
         TaskName: "Приготовить тортик",
         TaskSatus: "Make",
         TaskPriority: 2,
-        date: "2023-07-18",
+        date: "2023-07-16",
       },
       {
         id: 3,
         TaskName: "ГотовоГотовоГотовоГотово",
         TaskSatus: "Done",
         TaskPriority: 3,
-        date: "2023-07-18",
+        date: "2023-07-16",
+      },
+      {
+        id: 3,
+        TaskName: "ГотовоГотовоГотовоГотово",
+        TaskSatus: "Done",
+        TaskPriority: 3,
+        date: "2023-07-17",
       },
     ],
   },
